f5495ffb32a2a7eb6667fc44d172c705aa9fb4c5
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-12 08:32:13 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,6 +1,6 @@
 # Add your variables here
 
-
+=begin
 def calculator(x,y)
 
   first_number = x
@@ -21,3 +21,13 @@ def calculator(x,y)
 end
 
 calculator(1,2)
+
+=end
+
+first_number = 4
+second_number = 4
+
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
