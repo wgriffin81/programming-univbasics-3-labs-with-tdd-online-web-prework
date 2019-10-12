480db8cd292afac9b9579c424ce9a9cb545f122e
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-12 08:18:37 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,23 @@
 # Add your variables here
+
+
+def calculator(x,y)
+
+  first_number = x
+  second_number = y
+  
+  
+ sum = first_number + second_number
+ difference = first_number - second_number
+ product = first_number * second_number
+ quotient = first_number / second_number
+
+  
+  
+  puts sum 
+  puts difference 
+  puts product 
+  puts quotient 
+end
+
+calculator(1,2)
